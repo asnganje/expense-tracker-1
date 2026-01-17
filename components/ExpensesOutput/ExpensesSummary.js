e8b4ff46b11d3f87ae10ed4892634e8a23a1f44b
asnganje <abd.nganje@gmail.com>
2026-01-17 10:16:31 +0300

    add relevant components for displaying expenses content

---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+function ExpensesSummary() {
+  return(
+    <View>
+      <Text>Last 7 days</Text>
+      <Text>$177.96</Text>
+    </View>
+  )
+}
+
+export default ExpensesSummary;
