5ac5dc73e99e81a9492cef5cf618d4a83164f3d0
asnganje <abd.nganje@gmail.com>
2026-01-17 19:27:22 +0300

    add icon button for deleting with a dummy delete function

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,7 @@
 import { useLayoutEffect } from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
 
 function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId
@@ -10,13 +12,34 @@ function ManageExpense({ route, navigation }) {
       title: isEditing ? "Edit Expense" : "Add Expense" 
     })
   }, [navigation, isEditing])
+
+  function deleteExpenseHandler() {}
   
   return(
-    <View>
-      <Text>Expense Management</Text>
+    <View style={styles.container}>
+      {isEditing && 
+      <View style={styles.deleteContainer}>
+        <IconButton icon="trash" size={32} color={GlobalStyles.colors.error500} onPress={deleteExpenseHandler} />
+      </View>
+      }
     </View>
   )
   
 }
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    padding:24,
+    backgroundColor:GlobalStyles.colors.primary800
+  },
+  deleteContainer:{
+    marginTop:16,
+    paddingTop:8,
+    borderTopWidth:2,
+    borderTopColor:GlobalStyles.colors.primary200,
+    alignItems:"center"
+  }
+})
