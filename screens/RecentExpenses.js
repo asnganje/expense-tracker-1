3f67a9d6c7c6d655606cfbc79e08c7307d91fe5c
asnganje <abd.nganje@gmail.com>
2026-01-17 09:25:18 +0300

    add the basic setup for the app's main screens

---
+++ screens/RecentExpenses.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+function RecentExpense() {
+  return(
+    <View>
+      <Text>Recent Expenses</Text>
+    </View>
+  )
+  
+}
+
+export default RecentExpense;
